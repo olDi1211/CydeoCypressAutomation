f66392bcbb2d49227b88c7647a858f969e06035f
olDi1211 <116587153+olDi1211@users.noreply.github.com>
2023-05-06 17:46:29 +0200

    add first commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+
+    baseUrl: 'https://practice.cydeo.com/',
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/testStructure.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Context: My First Test',  () => {
+
+    before(() => {
+        //runs once before all test cases in this describe block, like beforeClass in TestNG
+    })
+
+
+    beforeEach(() => {
+        //run before each test case, beforeMethod in TestNG
+        cy.clearCookies;
+    })
+
+    after(() => {
+        //similar to afterClass in TestNG, runs once after all tests finished
+    })
+
+    afterEach(() => {
+        //similar to afterMethod in TestNG
+    })
+
+    it('Opening a web application', () =>{
+        cy.visit('/registration_form');
+        
+    })
+
+})
